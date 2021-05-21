Fixed lidar angle calculation

shipNavEnv/Bodies.py
@@ -14,7 +14,7 @@ class BodyType(Enum):
     TARGET = 3
 
 class Body:
-    def __init__(self, world, *args, **kwargs):
+    def __init__(self, world,*args, **kwargs):
         self.world = world
         self.body = None
         self.hit_with = []
@@ -64,8 +64,8 @@ class Body:
 class Obstacle(Body):
     DEFAULT_DIST = 1
     DEFAULT_BEARING = 0
-    def __init__(self, world, x, y, **kwargs):
-        super().__init__(world, x, y, **kwargs)
+    def __init__(self, world, position, **kwargs):
+        super().__init__(world, position, **kwargs)
         self.distance_to_ship = self.DEFAULT_DIST
         self.bearing_from_ship = self.DEFAULT_BEARING
         self.seen = False
@@ -83,8 +83,8 @@ class Obstacle(Body):
         return self.body.color1 if self.is_hit() else self.body.color2 if self.seen else self.body.color3
 
 class RoundObstacle(Obstacle):
-    def __init__(self, world, x, y, **kwargs):
-        super().__init__(world, x, y, **kwargs)
+    def __init__(self, world, position, **kwargs):
+        super().__init__(world, position, **kwargs)
 
     def render(self, viewer):
         trans = self.body.transform
@@ -120,16 +120,16 @@ class Ship(Body):
     K_Yv = 10*K_Xu              # [N/(m/s)]
 
 
-    def __init__(self, world, init_angle, init_x, init_y, obs_radius, **kwargs):
-        Body.__init__(self, world, init_angle, init_x, init_y, **kwargs)
+    def __init__(self, world, init_angle, position, obs_radius, **kwargs):
+        Body.__init__(self, world, init_angle, position, **kwargs)
         self.throttle = 0
         self.thruster_angle = 0
         self.type = BodyType.SHIP
         self.obs_radius = obs_radius
 
-    def _build(self, init_angle, init_x, init_y, **kwargs):
+    def _build(self, init_angle, position, **kwargs):
         self.body = self.world.CreateDynamicBody(
-                position=(init_x, init_y),
+                position=position,
                 angle=init_angle,
                 fixtures=fixtureDef(
                     shape=polygonShape(vertices=((-Ship.SHIP_WIDTH / 2, 0),
@@ -248,8 +248,8 @@ class Ship(Body):
         self.body.ApplyForce(force=force_damping, point=COGpos, wake=False)
 
 class ShipLidar(Ship):
-    def __init__(self, world, init_angle, init_x, init_y, nb_lidars, lidar_range, **kwargs):
-        Ship.__init__(self, world, init_angle, init_x, init_y, 0, **kwargs)
+    def __init__(self, world, init_angle, position, nb_lidars, lidar_range, **kwargs):
+        Ship.__init__(self, world, init_angle, position, 0, **kwargs)
         self.nb_lidars = nb_lidars
         self.lidar_range = lidar_range
         self.lidars = [LidarCallback(dont_report = [BodyType.TARGET]) for _ in range(self.nb_lidars)]
@@ -257,13 +257,13 @@ class ShipLidar(Ship):
 
     def _update_lidars(self):
         pos = self.body.position
-        angle = self.body.angle
+        angle = self.body.angle + np.pi/2
         for i, lidar in enumerate(self.lidars):
             lidar.fraction = 1.0
             lidar.p1 = pos
             lidar.p2 = (
-                    pos[0] + math.sin((angle + 2 * math.pi * i) / self.nb_lidars) * self.lidar_range,
-                    pos[1] - math.cos((angle + 2 * math.pi * i) / self.nb_lidars) * self.lidar_range)
+                    pos[0] + math.sin((2 * math.pi * i) / self.nb_lidars + angle) * self.lidar_range,
+                    pos[1] - math.cos((2 * math.pi * i) / self.nb_lidars + angle) * self.lidar_range)
             self.world.RayCast(lidar, lidar.p1, lidar.p2)
     
     def update(self):
@@ -275,13 +275,13 @@ class ShipLidar(Ship):
             viewer.draw_polyline( [lidar.p1, lidar.p2], color=rgb(255, 0, 0), linewidth=1)
 
 class ShipObstacle(Ship, Obstacle):
-    def __init__(self, world, init_angle, init_x, init_y, **kwargs):
+    def __init__(self, world, init_angle, position, **kwargs):
         #super(Obstacle).__init__(world, init_x, init_y)
         Obstacle.clean(self)
-        Ship.__init__(self, world, init_angle, init_x, init_y, 0, **kwargs)
+        Ship.__init__(self, world, init_angle, position, 0, **kwargs)
 
-    def _build(self, init_angle, init_x, init_y, **kwargs):
-        Ship._build(self, init_angle, init_x, init_y, **kwargs)
+    def _build(self, *args, **kwargs):
+        Ship._build(self, *args, **kwargs)
         self.body.color1 = rgb(83, 43, 9) # brown
         self.body.color2 = rgb(41, 14, 9) # darker brown
         self.body.color3 = rgb(255, 255, 255) # seen
@@ -305,15 +305,15 @@ class ShipObstacle(Ship, Obstacle):
     
 class Rock(RoundObstacle):
     RADIUS = 20
-    def __init__(self, world, x, y, **kwargs):
-        super().__init__(world, x, y, **kwargs)
+    def __init__(self, world, position, **kwargs):
+        super().__init__(world, position, **kwargs)
         self.type = BodyType.ROCK
 
-    def _build(self, x, y, **kwargs):
+    def _build(self, position, **kwargs):
         radius = np.random.uniform(0.5*Rock.RADIUS,2*Rock.RADIUS)
 
         self.body = self.world.CreateStaticBody(
-            position=(x, y), # FIXME Should have something like: map.get_random_available_position()
+            position=position, # FIXME Should have something like: map.get_random_available_position()
             angle=np.random.uniform( 0, 2*math.pi), # FIXME Not really useful if circle shaped
             fixtures=fixtureDef(
             shape = circleShape(pos=(0,0),radius = radius),
@@ -330,13 +330,13 @@ class Rock(RoundObstacle):
 
 class Target(RoundObstacle):
     RADIUS = 20
-    def __init__(self, world, x, y, **kwargs):
-        super().__init__(world, x, y, **kwargs)
+    def __init__(self, world, position, **kwargs):
+        super().__init__(world, position, **kwargs)
         self.type = BodyType.TARGET
 
-    def _build(self, x, y, **kwargs):
+    def _build(self, position, **kwargs):
         self.body =  self.world.CreateStaticBody(
-            position = (x, y),
+            position = position,
             angle = 0.0,
             fixtures = fixtureDef(
             shape = circleShape(pos=(0,0), radius = Target.RADIUS),

shipNavEnv/Worlds.py
@@ -21,15 +21,15 @@ class World:
         self.viewer = None
         self.populate()
 
-    def _build_ship(self, angle, x=0, y=0):
-        return Ship(self.world, angle, x, y, **self.ship_kwargs if self.ship_kwargs else dict())
+    def _build_ship(self, angle, position=(0,0)):
+        return Ship(self.world, angle, position, **self.ship_kwargs if self.ship_kwargs else dict())
     
     def populate(self):
         angle = self.get_random_angle()
         self.ship = self._build_ship(angle)
         self.get_random_free_space(self.ship)
 
-        self.target = Target(self.world, 0, 0)
+        self.target = Target(self.world, (0, 0))
         self.get_random_free_space(self.target)
 
     def reset(self):
@@ -59,6 +59,8 @@ class World:
     def get_random_angle(self):
         return np.random.uniform(0, 2 * math.pi)
 
+    # FIXME According to Box2D doc Caution: Do not create a body at the origin and then move it. If you create several bodies at the origin, then performance will suffer.
+    # Fix idea -> use functools partial to create body with position and return it (or destroy it)
     def get_random_free_space(self, body : Body, trial = 0, limit = 100):
         if trial == limit:
             return False # FIXME (maybe take something outside world border
@@ -197,8 +199,8 @@ class RockOnlyWorld(World):
 
     def populate(self):
         for i in range(self.n_rocks):
-            x, y = self.get_random_pos(scale = self.rock_scale)
-            rock = Rock(self.world, x, y)
+            pos = self.get_random_pos(scale = self.rock_scale)
+            rock = Rock(self.world, pos)
             self.rocks.append(rock)
 
         World.populate(self)
@@ -208,8 +210,8 @@ class RockOnlyWorldLidar(RockOnlyWorld):
         self.n_lidars = n_lidars
         RockOnlyWorld.__init__(self, n_rocks, rock_scale, ship_kwargs)
 
-    def _build_ship(self, angle, x=0, y=0):
-        return ShipLidar(self.world, angle, x, y, self.n_lidars, np.maximum(self.WIDTH, self.HEIGHT), **self.ship_kwargs if self.ship_kwargs else dict())
+    def _build_ship(self, angle, position=(0,0)):
+        return ShipLidar(self.world, angle, position, self.n_lidars, np.maximum(self.WIDTH, self.HEIGHT), **self.ship_kwargs if self.ship_kwargs else dict())
 
 
 class ShipsOnlyWorld(World):
@@ -219,9 +221,9 @@ class ShipsOnlyWorld(World):
 
     def populate(self):
         for i in range(self.n_ships):
-            x, y = self.get_random_pos()
+            pos = self.get_random_pos()
             angle = self.get_random_angle()
-            ship = ShipObstacle(self.world, angle, x, y)
+            ship = ShipObstacle(self.world, angle, pos)
             self.ships.append(ship)
 
         super().populate()

shipNavEnv/envs/ShipNavRocks.py
@@ -48,6 +48,7 @@ Discrete control inputs are:
 class ShipNavRocks(gym.Env):
     MAX_STEPS = 1000    # max steps for a simulation
     FPS = 60            # simulation framerate
+    SHIP_STATE_LENGTH = 6
 
     def __init__(self,**kwargs):
         
@@ -83,7 +84,7 @@ class ShipNavRocks(gym.Env):
         return RockOnlyWorld(self.n_rocks, self.rock_scale, {'obs_radius': self.obs_radius})
 
     def _get_obs_space(self):
-        return spaces.Box(-1.0,1.0,shape=(4 + 2 * self.n_rocks_obs,), dtype=np.float32)
+        return spaces.Box(-1.0,1.0,shape=(self.SHIP_STATE_LENGTH + 2 * self.n_rocks_obs,), dtype=np.float32)
 
 
     def _read_kwargs(self, **kwargs):
@@ -148,6 +149,7 @@ class ShipNavRocks(gym.Env):
         obstacles = self.world.get_obstacles()
         # sort rocks from closest to farthest
         obstacles.sort(key=lambda x: (0 if x.seen else 1, x.distance_to_ship))
+        #print([obstacle.seen for obstacle in obstacles])
 
         new_seen = []
         for obs in obstacles:
@@ -155,7 +157,6 @@ class ShipNavRocks(gym.Env):
                 new_seen.append(obs)
             
         new_obstacles = []
-        #print("new seen %s" % new_seen)
         for obs in self.obstacles:
             if not obs.seen and new_seen: # If old obstacle seen is not seen anymore, and some new to add
                 new_obstacles.append(new_seen[0]) #Add closest one instead of a random one
@@ -181,7 +182,9 @@ class ShipNavRocks(gym.Env):
         ship = self.world.ship
 
         state = []
-        #state.append(ship.body.linearVelocity)
+        velocity_x, velocity_y = ship.body.GetLocalVector(ship.body.linearVelocity)
+        state.append(velocity_x / ship.Vmax)
+        state.append(velocity_y / ship.Vmax)
         state.append(ship.body.angularVelocity/ship.Rmax)
         state.append(ship.thruster_angle / ship.THRUSTER_MAX_ANGLE)
         state.append(self.world.get_ship_target_standard_dist())
@@ -218,7 +221,7 @@ class ShipNavRocks(gym.Env):
                 reward -= 5 #high negative reward. hitting anything else than target is bad
             done = True
         else:   # general case, we're trying to reach target so being close should be rewarded
-            reward -= 1/ self.MAX_STEPS
+            reward -= 2/ self.MAX_STEPS
             #self.reward = - ((2* distance_t / norm_pos)  - 1) / self.self.MAX_STEPS # FIXME Macro instead of magic number
             #print(self.reward)
         
@@ -242,9 +245,10 @@ class ShipNavRocks(gym.Env):
         #if self.stepnumber == 1:
         #    print(len(self.state))
         #print(ship.body.linearVelocity)
+        #print(ship.body.GetLocalVector(ship.body.linearVelocity))
         #print(np.sqrt(ship.body.linearVelocity[0]**2 + ship.body.linearVelocity[1]**2))
         #if not (self.stepnumber % self.FPS):
-        #    print(self.state)
+        print(self.state)
 
         # Normalized ship states
         #state += list(np.asarray(self.ship.body.GetLocalVector(self.ship.body.linearVelocity))/Ship.Vmax)
@@ -317,7 +321,7 @@ class ShipNavRocksLidar(ShipNavRocks):
         return RockOnlyWorldLidar(self.n_rocks, self.n_lidars, self.rock_scale)
 
     def _get_obs_space(self):
-        return spaces.Box(-1.0,1.0,shape=(4 +  self.n_lidars,), dtype=np.float32)
+        return spaces.Box(-1.0,1.0,shape=(self.SHIP_STATE_LENGTH +  self.n_lidars,), dtype=np.float32)
     
     def _get_state(self):
         ship = self.world.ship