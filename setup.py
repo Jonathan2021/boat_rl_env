01b3ada587719134269f29539990242edfbdc4e0
Jonathan Sands <jonathan.sands@edu.devinci.fr>
2021-09-03 18:53:18 +0200

    Updated setup dependencies

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='shipNavEnv',
       version='1.1.1',
-      install_requires=['gym','numpy','Box2D', 'PyVirtualDisplay']#And any other dependencies required
+      install_requires=['gym','numpy','pybox2d', 'PyVirtualDisplay', 'pyyaml', 'pyglet', 'optuna', 'pathfinding']#And any other dependencies required
 )
